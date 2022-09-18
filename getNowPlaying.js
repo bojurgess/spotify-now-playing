dbf0bfabccba39ea6c569ec2b40d1719a672c797
bojurgess <benodragon@outlook.com>
2022-09-18 16:44:30 +0100

    ✨ Add getNowPlaying function
    Added a function in a getNowPlaying.js file which fetches the playback state of the user and if music is playing logs the artist and song name to an object

---
+++ getNowPlaying.js
@@ -0,0 +1,29 @@
+const { clientId, clientSecret, redirectUri } = require('./credentials.json');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret,
+  redirectUri: redirectUri
+});
+
+exports.nowPlaying = {};
+
+function getNowPlaying() {
+  spotifyApi.getMyCurrentPlaybackState().then(
+    function(data) {
+      if (data.body && data.body.is_playing) {
+        console.log('I am currently listening to: ' + data.body.item.name, 'by ' + data.body.item.artists[0].name);
+        nowPlaying.songName = data.body.item.name;
+        nowPlaying.artistName = data.body.item.artists[0].name;
+      }
+      else {
+        console.log('I am not currently listening to anything.');
+        nowPlaying.songName = 'I am not currently listening to anything.';
+      }
+    },
+    function(err) {
+      console.log('Error:' + err);
+    }
+  )
+};

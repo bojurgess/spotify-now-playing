2a97d5b443654a65845b826d19f906af3f9f1e94
bojurgess <benodragon@outlook.com>
2022-09-18 15:42:36 +0100

    🎉 Initial Commit
    Created the generateURL.js file, which creates an auth token and stores it in a json file

---
+++ json.js
@@ -0,0 +1,3 @@
+const credentials = {clientId:"19cbeea0ef414355a721b42197f42b94",clientSecret:"7c9d84839afa4c2f9787529f926e4758",redirectUri:"http://localhost:8888/callback"};
+var jsonString = JSON.stringify(credentials);
+console.log(jsonString);

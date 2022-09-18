385d59dc6d8a55cb3c52e68fcd8a3083e8b9a5e6
bojurgess <benodragon@outlook.com>
2022-09-18 16:34:39 +0100

    ♻️  Turn token generation into a function
    Turned the block of code that generates an access token into a function

--- authenticate.js
+++ authenticate.js
@@ -9,6 +9,7 @@ const spotifyApi = new SpotifyWebApi({
   redirectUri: redirectUri
 });
 
+function getTokens () {
 spotifyApi.authorizationCodeGrant(code).then(
   function(data) {
     console.log('Authentication Success! Access Token is ' + data.body['access_token'])
@@ -19,15 +20,19 @@ spotifyApi.authorizationCodeGrant(code).then(
   function(err) {
     console.log('Uh Oh! Unexpected Error Occurred. ' + err);
   }
-);
+)
+};
 
 function refreshAccessToken() {
   spotifyApi.refreshAccessToken().then (
     function(data) {
       console.log('Successfully Refreshed Access Token!');
+      spotifyApi.setAccessToken(data.body['access_token']);
     }
   ),
     function(err) {
       console.log('Uh Oh! Unexpected Error Occured. ' + err);
     }
 };
+
+getTokens()

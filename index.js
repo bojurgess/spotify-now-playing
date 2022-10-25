e2fa548ff4290aa90dd1259dde646307890f22ba
bojurgess <benodragon@outlook.com>
2022-10-25 17:42:41 +0100

    Add generation of access and refresh tokens

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv'
 import QueryString from 'qs'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import request from 'request';
 
 //Initialise dotenv & express
 dotenv.config()
@@ -39,23 +40,56 @@ app.get('/callback', (req, res) => {
     //Parses the state passed to us in the callback url as a number, if a state is not provided it is defined as null
     state.callback = Number(req.query.state) || null
 
-    //If callback state is null, redirect to /# and provide ?error=state_mismatch
-    if (state.callback === null) {
+    const code = req.query.code || null
+
+    //If callback state is null or does not match initial state, redirect to /# and provide ?error=state_mismatch
+    if (state.callback === null || state.callback !== state.initial) {
         res.redirect('/#' + 
         QueryString.stringify({
             error: 'state_mismatch'
         }));
+        console.log('State mismatch detected! Aborting authentication...')
     }
     //If callback state matches the initial state, send a success.html file and proceed with auth process.
     else if (state.callback === state.initial) {
-        res.send('/success.html', {root: __dirname});
-    }
-    //If callback state does not match the initial state, redirect to /# and provide querystring ?error=state_mismatch
-    else if (state.callback !== state.initial) {
-        res.redirect('/#' +
-        QueryString.stringify({
-            error: 'state_mismatch'
-        }));
+        res.sendFile('/success.html', {root: __dirname});
+        console.log('Matching states found! Proceeding with auth process.')
+
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: process.env.REDIRECT_URI,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')),
+                'Content-Type': 'applictation/x-www-form-urlencoded'
+            },
+            json: true
+        };
+
+        request.post(authOptions, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const access_token = body.access_token,
+                      refresh_token = body.refresh_token;
+                
+                const options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                request.get(options, (error, response, body) => {
+                    console.log(body)
+                });
+            } else {
+                res.redirect('/#' + 
+                    QueryString.stringify({
+                        error: 'invalid_token'
+                }));
+            }
+        })
     }
 });
 

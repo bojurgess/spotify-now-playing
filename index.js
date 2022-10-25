4aedb3f6c06e8c1c8eb87184a84f394b37f6e4dc
bojurgess <benodragon@outlook.com>
2022-10-25 17:54:53 +0100

    Add refresh token generation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Dependencies
+// Dependencies
 import express from 'express'
 import * as dotenv from 'dotenv'
 import QueryString from 'qs'
@@ -6,17 +6,17 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import request from 'request';
 
-//Initialise dotenv & express
+// Initialise dotenv & express
 dotenv.config()
 const app = express()
-//Initialise empty 'state' object for later use
+// Initialise empty 'state' object for later use
 const state = {}
 
-//Defining dirname
+// Defining dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//Login route, passes basic information to Spotify Accounts Service.
+// Login route, passes basic information to Spotify Accounts Service.
 app.get('/login', (req, res) => {
 
     //Random Number Generator
@@ -37,12 +37,12 @@ app.get('/login', (req, res) => {
 //Callback route post initial authentication stage with Spotify Accounts Service
 app.get('/callback', (req, res) => {
     
-    //Parses the state passed to us in the callback url as a number, if a state is not provided it is defined as null
+    // Parses the state passed to us in the callback url as a number, if a state is not provided it is defined as null
     state.callback = Number(req.query.state) || null
 
     const code = req.query.code || null
 
-    //If callback state is null or does not match initial state, redirect to /# and provide ?error=state_mismatch
+    // If callback state is null or does not match initial state, redirect to /# and provide ?error=state_mismatch
     if (state.callback === null || state.callback !== state.initial) {
         res.redirect('/#' + 
         QueryString.stringify({
@@ -50,12 +50,12 @@ app.get('/callback', (req, res) => {
         }));
         console.log('State mismatch detected! Aborting authentication...')
     }
-    //If callback state matches the initial state, send a success.html file and proceed with auth process.
+    // If callback state matches the initial state, send a success.html file and proceed with auth process.
     else if (state.callback === state.initial) {
         res.sendFile('/success.html', {root: __dirname});
         console.log('Matching states found! Proceeding with auth process.')
 
-        //Defining what is sent to the accounts service in the post request
+        // Defining what is sent to the accounts service in the post request
         const authOptions = {
             url: 'https://accounts.spotify.com/api/token',
             form: {
@@ -70,7 +70,7 @@ app.get('/callback', (req, res) => {
             json: true
         };
 
-        //Making post request to Spotify Accounts Service
+        // Making post request to Spotify Accounts Service
         request.post(authOptions, (error, response, body) => {
             //If the expected 200 response code is returned, define the access & refresh tokens.
             if (!error && response.statusCode === 200) {
@@ -88,15 +88,41 @@ app.get('/callback', (req, res) => {
                 request.get(options, (error, response, body) => {
                     console.log(body)
                 });
-            //If an unexpected response code is returned in the post request, redirect to /# and pass querystring 'invalid_token'.
+            // If an unexpected response code is returned in the post request, redirect to /# and pass querystring 'invalid_token'.
             } else {
                 res.redirect('/#' + 
                     QueryString.stringify({
                         error: 'invalid_token'
                 }));
             }
-        })
+        });
     }
+});
+
+// Request a new access token using our refresh token.
+app.get('/refresh_token', (req, res) => {
+    const refresh_token = req.query.refresh_token;
+    // Defining authOptions to be passed to our post request. Same as before except we discard the need for a code value and use the grant type 'refresh_token' instead.
+    // We also discard the 'content_type' header in our new request.
+    const authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        },
+        headers: { 'Authorization': 'Basic ' + (Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')), },
+        json: true
+    };
+
+    // Make our post request to the accounts service, providing our authOptions to generate a new access token.
+    request.post(authOptions, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const access_token = body.access_token;
+            res.send({
+                'access_token': access_token
+            });
+        }
+    });
 });
 
 //Tells express app to listen on port defined in .env then logs the address to the console.

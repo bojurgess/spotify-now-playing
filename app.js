f25a8d6289e2abecdd686833db8b57ffd130858f
bojurgess <benodragon@outlook.com>
2022-10-26 01:28:54 +0100

    Finish refactoring of routes into router.js
    Moved all of the routes from app.js to router.js and imported them back into app.js as middleware.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Dependencies
 import express, { application } from 'express'
 import * as dotenv from 'dotenv'
-import QueryString from 'qs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import request from 'request';
 import cors from 'cors';
+
+import router from './routes.js';
 
 // Initialise dotenv & express
 dotenv.config()
@@ -14,95 +15,15 @@ app.use(cors())
 
 express.static.mime.define({'application/javascript': ['js']});
 // Initialise empty 'state' object for later use
-const state = {}
+export const state = {}
 export const spotify = {}
 
 // Defining dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Login route, passes basic information to Spotify Accounts Service.
-app.get('/login', (req, res) => {
-
-    //Random Number Generator
-    state.initial = Math.floor(Math.random() * 16)
-    const scope = 'user-read-currently-playing';
-
-    res.redirect('https://accounts.spotify.com/authorize?' + 
-        QueryString.stringify({
-            response_type: 'code',
-            client_id: process.env.CLIENT_ID,
-            scope: scope,
-            redirect_uri: process.env.REDIRECT_URI,
-            state: state.initial,
-            show_dialog: true
-    }));
-})
-
-//Callback route post initial authentication stage with Spotify Accounts Service
-app.get('/callback', (req, res) => {
-    
-    // Parses the state passed to us in the callback url as a number, if a state is not provided it is defined as null
-    state.callback = Number(req.query.state) || null
-
-    const code = req.query.code || null
-
-    // If callback state is null or does not match initial state, redirect to /# and provide ?error=state_mismatch
-    if (state.callback === null || state.callback !== state.initial) {
-        res.redirect('/#' + 
-        QueryString.stringify({
-            error: 'state_mismatch'
-        }));
-        console.log('State mismatch detected! Aborting authentication...')
-    }
-    // If callback state matches the initial state, send a success.html file and proceed with auth process.
-    else if (state.callback === state.initial) {
-        res.redirect('/NowPlaying');
-        console.log('Matching states found! Proceeding with auth process.')
-
-        // Defining what is sent to the accounts service in the post request
-        const authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
-            form: {
-                code: code,
-                redirect_uri: process.env.REDIRECT_URI,
-                grant_type: 'authorization_code'
-            },
-            headers: {
-                'Authorization': 'Basic ' + (Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')),
-                'Content-Type': 'applictation/x-www-form-urlencoded'
-            },
-            json: true
-        };
-
-        // Making post request to Spotify Accounts Service
-        request.post(authOptions, (error, response, body) => {
-            //If the expected 200 response code is returned, define the access & refresh tokens.
-            if (!error && response.statusCode === 200) {
-
-                spotify.access_token = body.access_token;
-                spotify.refresh_token = body.refresh_token;
-                
-                //Defining options to be sent in another get request.
-                const options = {
-                    url: 'https://api.spotify.com/v1/me',
-                    headers: { 'Authorization': 'Bearer ' + spotify.access_token },
-                    json: true
-                };
-
-                //Make get request to access the Spotify Web API, using previously acquired access token.
-                request.get(options, (error, response, body) => {
-                });
-            // If an unexpected response code is returned in the post request, redirect to /# and pass querystring 'invalid_token'.
-            } else {
-                res.redirect('/#' + 
-                    QueryString.stringify({
-                        error: 'invalid_token'
-                }));
-            }
-        });
-    }
-});
+// Import our router from routes.js
+app.use(router)
 
 // Request a new access token using our refresh token.
 function refresh_token() {
--- routes.js
+++ routes.js
@@ -1,30 +1,33 @@
 // Dependencies
-import path from 'path'
-import { fileURLToPath } from 'url'
-import express, { application } from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { state, spotify } from './app.js';
+import request from 'request';
+import QueryString from 'qs';
+
+import express, { application } from 'express';
 
 // Defining dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Initialise express library, tell it to use cors and define our mime type for js files as application/javascript
-const app = express()
-app.use(cors())
+const router = express.Router()
 
 express.static.mime.define({'application/javascript': ['js']});
 
-// Add a route for /
-app.get('/', (req, res) => {
+// Route for /
+router.get('/', (req, res) => {
     res.sendFile('./routes/index.html', {root: __dirname})
 })
 
-// Add a route for /NowPlaying
-app.get('/NowPlaying', (req, res) => {
+// Route for /NowPlaying
+router.get('/NowPlaying', (req, res) => {
     res.sendFile('./routes/nowplaying.html', {root: __dirname})
 })
 
 // Login route, passes basic information to Spotify Accounts Service.
-app.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
 
     //Random Number Generator
     state.initial = Math.floor(Math.random() * 16)
@@ -40,3 +43,69 @@ app.get('/login', (req, res) => {
             show_dialog: true
     }));
 })
+//Callback route post initial authentication stage with Spotify Accounts Service
+router.get('/callback', (req, res) => {
+    
+    // Parses the state passed to us in the callback url as a number, if a state is not provided it is defined as null
+    state.callback = Number(req.query.state) || null
+
+    const code = req.query.code || null
+
+    // If callback state is null or does not match initial state, redirect to /# and provide ?error=state_mismatch
+    if (state.callback === null || state.callback !== state.initial) {
+        res.redirect('/#' + 
+        QueryString.stringify({
+            error: 'state_mismatch'
+        }));
+        console.log('State mismatch detected! Aborting authentication...')
+    }
+    // If callback state matches the initial state, send a success.html file and proceed with auth process.
+    else if (state.callback === state.initial) {
+        res.redirect('/NowPlaying');
+        console.log('Matching states found! Proceeding with auth process.')
+
+        // Defining what is sent to the accounts service in the post request
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: process.env.REDIRECT_URI,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')),
+                'Content-Type': 'applictation/x-www-form-urlencoded'
+            },
+            json: true
+        };
+
+        // Making post request to Spotify Accounts Service
+        request.post(authOptions, (error, response, body) => {
+            //If the expected 200 response code is returned, define the access & refresh tokens.
+            if (!error && response.statusCode === 200) {
+
+                spotify.access_token = body.access_token;
+                spotify.refresh_token = body.refresh_token;
+
+                //Defining options to be sent in another get request.
+                const options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + spotify.access_token },
+                    json: true
+                };
+
+                //Make get request to access the Spotify Web API, using previously acquired access token.
+                request.get(options, (error, response, body) => {
+                });
+            // If an unexpected response code is returned in the post request, redirect to /# and pass querystring 'invalid_token'.
+            } else {
+                res.redirect('/#' + 
+                    QueryString.stringify({
+                        error: 'invalid_token'
+                }));
+            }
+        });
+    }
+});
+
+export default router
